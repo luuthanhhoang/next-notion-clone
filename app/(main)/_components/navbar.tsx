4ee1d70d03fbd16fd42c5f22397b946dd463ef44
luuthanhhoang <hoangthanhluu1998@gmail.com>
2024-07-30 16:20:24 +0700

    publish doc

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -7,6 +7,7 @@ import React from "react";
 import { Title } from "./title";
 import Banner from "./banner";
 import Menu from "./menu";
+import Publish from "./publish";
 
 type Props = {
   isCollapsed: boolean;
@@ -23,6 +24,7 @@ const Navbar = ({ isCollapsed, onResetWidth }: Props) => {
     return (
       <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
         <Title.Skeleton />
+
         <div className="flex items-center gap-x-2">
           <Menu.Skeleton />
         </div>
@@ -47,6 +49,7 @@ const Navbar = ({ isCollapsed, onResetWidth }: Props) => {
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
           <div className="flex items-center gap-x-2">
+            <Publish initialData={document} />
             <Menu documentId={document._id} />
           </div>
         </div>
